156fc4e716b6dc031879f7c6303448ec3565bd53
Stefan Schmidt <stefan7schmidt@gmx.net>
2019-03-09 21:09:50 +0100

    file names harmonized.

--- part1/99-99_What_I_learned.py
+++ part1/99-99_What_I_learned.py
@@ -8,4 +8,12 @@
 squares = [value**2 for value in range(1, 11)]
 print(squares)
 
-# 2.
+# 2. Tuples
+# tuples are similar to lists, but are immutable.
+immutables = ('soil', 'wind', 'fire')
+print(immutables)
+# fails with TypeError:
+#immutables[0] = 'earth'
+# re-creation of the complete tuple is fine
+immutables = ('earth', 'wind', 'fire')
+print(immutables)
