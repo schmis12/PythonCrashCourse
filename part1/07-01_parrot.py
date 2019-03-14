b586557dd7fdc3f5a3d2f5df27fb1f43d7394e62
Stefan Schmidt <stefan7schmidt@gmx.net>
2019-03-14 16:58:36 +0100

    chapter 7 started

---
+++ part1/07-01_parrot.py
@@ -0,0 +1,4 @@
+# Stefan J. Schmidt, 14.03.2019
+
+message = input("Tell me something and I will repeat it back to you: ")
+print(message)
---
+++ part1/07-02_greeter.py
@@ -0,0 +1,7 @@
+# Stefan J. Schmidt, 14.03.2019
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"Hello, {name}!")
